ffbf645f562504ec34970f6af75b4b10c36fc69a
Nace Tavčer <46198891+hxllv@users.noreply.github.com>
2023-01-06 08:37:44 +0100

    api

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -9,10 +9,37 @@ class FriendController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth:sanctum', 'auth']);
     }
 
-    public function index() {
+    public function friendsApi()
+    {
+        $friendsOfMine = auth()->user()->friendsOfMine()->wherePivot('accepted', '=', 1)->get();
+        $friendsOf = auth()->user()->friendsOf()->wherePivot('accepted', '=', 1)->get();
+
+        //$friendRequests = auth()->user()->friendsOf()->wherePivot('accepted', '=', 0)->get();
+
+        $friends = $friendsOfMine->merge($friendsOf);
+
+        return response()->json($friends);
+        //return view('friends')->with('friends', $friends)->with('friendRequests', $friendRequests);
+    }
+
+    public function requestsApi()
+    {
+        /* $friendsOfMine = auth()->user()->friendsOfMine()->wherePivot('accepted', '=', 1)->get();
+        $friendsOf = auth()->user()->friendsOf()->wherePivot('accepted', '=', 1)->get(); */
+
+        $friendRequests = auth()->user()->friendsOf()->wherePivot('accepted', '=', 0)->get();
+
+        /* $friends = $friendsOfMine->merge($friendsOf); */
+
+        return response()->json($friendRequests);
+        //return view('friends')->with('friends', $friends)->with('friendRequests', $friendRequests);
+    }
+
+    public function index()
+    {
         $friendsOfMine = auth()->user()->friendsOfMine()->wherePivot('accepted', '=', 1)->get();
         $friendsOf = auth()->user()->friendsOf()->wherePivot('accepted', '=', 1)->get();
 
@@ -36,10 +63,25 @@ class FriendController extends Controller
         return redirect('/' . $page . '/' . ($page == 'chat-profile' ? $user : ''));
     }
 
+    public function delete2($user)
+    {
+        auth()->user()->friendsOfMine()->detach($user);
+        auth()->user()->friendsOf()->detach($user);
+
+        return 0;
+    }
+
     public function update($user, $page)
     {
         auth()->user()->friendsOf()->wherePivot('user_id', '=', $user)->update(['accepted' => 1]);
 
         return redirect('/' . $page . '/' . ($page == 'chat-profile' ? $user : ''));
+    }
+
+    public function update2($user, $page)
+    {
+        auth()->user()->friendsOf()->wherePivot('user_id', '=', $user)->update(['accepted' => 1]);
+
+        return 0;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'store']);
+Route::get('/friends', [App\Http\Controllers\FriendController::class, 'friendsApi']);
+Route::get('/requests', [App\Http\Controllers\FriendController::class, 'requestsApi']);
+Route::get('/search', [App\Http\Controllers\SearchController::class, 'store']);
+Route::post('/friend/{user}', [App\Http\Controllers\FriendController::class, 'store']);
+Route::delete('/friend/{user}/{page}', [App\Http\Controllers\FriendController::class, 'delete2']);
+Route::patch('/friend/{user}/{page}', [App\Http\Controllers\FriendController::class, 'update2']);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
